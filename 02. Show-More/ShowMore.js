f732e52c45eef1868ee0b07c639ff937254203ee
Georgi Todorov <122493113+GeorgiTodorov961@users.noreply.github.com>
2023-03-22 10:39:58 +0100

    Delete 02. Show-More directory

--- 02. Show-More/ShowMore.js
+++
@@ -1,6 +0,0 @@
-function showText() {
-    const anchor = document.getElementById('more');
-    const span = document.getElementById('text');
-    span.style.display = 'inline';
-    anchor.style.display = 'none'
-}
