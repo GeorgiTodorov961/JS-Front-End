2800512198ac157fdd063e930b78d49652284d1c
Georgi Todorov <122493113+GeorgiTodorov961@users.noreply.github.com>
2023-03-22 10:40:14 +0100

    Delete 03. Collect-List-Items directory

--- 03. Collect-List-Items/CollectListItems.js
+++
@@ -1,9 +0,0 @@
-function extractText() {
-    const liElements = Array.from(document.querySelectorAll('#items > li'));
-    const result = document.getElementById('result')
-    liElements.
-    forEach((li) => {
-        result.textContent += li.textContent + '\n';
-    })
-
-}
